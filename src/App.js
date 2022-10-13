0f71f3694167cec6afd07834d8727ed6bdbd32f2
Cem <cemkgrbz@gmail.com>
2022-10-13 16:49:19 +0200

    added solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import RandomNumber from "./RandomNumber"
 
 export default function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <RandomNumber />
         </div>
     )
 }
